88de25f19938668ef19638f58b67bd5f6e1d213e
Arturo Salazar <arturo.v.salazar.jr@gmail.com>
2019-02-07 11:21:57 -0600

    modify to add todo objects and work with them
    modify code so add todo will create an object with two properties (todoText and completed) instead of just pushing text. 
    changeTodo now targest and changes the text on a todo object instead of just text
    toggleCompleted created to toggle completed property on a todo object

--- script.js
+++ script.js
@@ -1,23 +1,34 @@
 //hold todo list and related todo list methods in an object
 let todoList = {
-  todos: ['item1', 'item2', 'item3'],
+  todos: [],
 
   displayTodos: function() {
     console.log(`My Todos: `, this.todos);
   },
-
-  addTodo: function(todo) {
-    this.todos.push(todo);
+  //update to add a todo object with text + completed property instead of just text
+  addTodo: function(todoText) {
+    this.todos.push({
+      todoText: todoText,
+      completed: false
+    });
     this.displayTodos();
   },
-
+  // update so that function changes the todoText property on todo objects
   changeTodo: function(index, newVal) {
-    this.todos[index] = newVal;
+    this.todos[index].todoText = newVal;
     this.displayTodos();
   },
 
   deleteTodo: function(position) {
     this.todos.splice(position, 1);
+    this.displayTodos();
+  },
+  // create function to toggle completed property on todo objects
+  toggleCompleted: function(position) {
+    //create a variable to hold a reference to particular todo in question
+    //less clutter and easier to read code
+    let todo = this.todos[position];
+    todo.completed = !todo.completed;
     this.displayTodos();
   }
 };
