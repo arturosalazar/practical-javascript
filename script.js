d0978f02ab42699c6b0fcf8610c2b619c2c0d882
Arturo Salazar <arturo.v.salazar.jr@gmail.com>
2019-02-07 10:38:41 -0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,22 @@
+let todoList = {
+  todos: ['item1', 'item2', 'item3'],
+
+  displayTodos: function() {
+    console.log(`My Todos: `, this.todos);
+  },
+
+  addTodo: function(todo) {
+    this.todos.push(todo);
+    this.displayTodos();
+  },
+
+  changeTodo: function(index, newVal) {
+    this.todos[index] = newVal;
+    this.displayTodos();
+  },
+
+  deleteTodo: function(position) {
+    this.todos.splice(position, 1);
+    this.displayTodos();
+  }
+};
