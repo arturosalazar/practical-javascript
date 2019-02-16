364276a995c56f3519f71c7fa0377731af726fda
Arturo Salazar <arturo.v.salazar.jr@gmail.com>
2019-02-16 15:38:13 -0600

    remove todoList.displayTodos method that displayed to console
    removed method to display to console, update all handlers methods to display to the screen any time something is updated

--- script.js
+++ script.js
@@ -1,41 +1,20 @@
 //hold todo list and related todo list methods in an object
 let todoList = {
   todos: [],
-  //console.log the todoText for each element in our array of todos
-  //display if the todo is completed or not
-  displayTodos: function() {
-    //test if array is empty
-    if (this.todos.length === 0) {
-      console.log(`You don't have any todos!`);
-    } else {
-      console.log("My todos:");
-      for (let todo of this.todos) {
-        //change display depending on if the todo is completed
-        if (todo.completed === true) {
-          console.log(`(X) ${todo.todoText}`);
-        } else {
-          console.log(`( ) ${todo.todoText}`);
-        }
-      }
-    }
-  },
-  //update to add a todo object with text + completed property instead of just text
+  //add a todo object with text + completed property instead of just text
   addTodo: function(todoText) {
     this.todos.push({
       todoText: todoText,
       completed: false
     });
-    this.displayTodos();
   },
   // update so that function changes the todoText property on todo objects
   changeTodo: function(index, newVal) {
     this.todos[index].todoText = newVal;
-    this.displayTodos();
   },
 
   deleteTodo: function(position) {
     this.todos.splice(position, 1);
-    this.displayTodos();
   },
   // create function to toggle completed property on todo objects
   toggleCompleted: function(position) {
@@ -43,7 +22,6 @@ let todoList = {
     //less clutter and easier to read code
     let todo = this.todos[position];
     todo.completed = !todo.completed;
-    this.displayTodos();
   },
   //if everything is true, make everything false
   //else make everything true
@@ -64,7 +42,6 @@ let todoList = {
         todo.completed = true;
       }
     }
-    this.displayTodos();
   }
 };
 
@@ -79,6 +56,7 @@ let handlers = {
     todoList.addTodo(addTodosTextInput.value);
     //clear text box when done with running method
     addTodosTextInput.value = '';
+    view.displayTodos();
   },
   changeTodo: function() {
     let changeTodoPositionInput = document.getElementById('changeTodoPositionInput');
@@ -86,19 +64,23 @@ let handlers = {
     todoList.changeTodo(changeTodoPositionInput.valueAsNumber, changeTodoTextInput.value);
     changeTodoPositionInput.value = '';
     changeTodoTextInput.value = '';
+    view.displayTodos();
   },
   deleteTodo: function() {
     let deleteTodoPositionInput = document.getElementById('deleteTodoPositionInput');
     todoList.deleteTodo(deleteTodoPositionInput.valueAsNumber);
     deleteTodoPositionInput.value = '';
+    view.displayTodos();
   },
   toggleCompleted: function() {
     let toggleTodoPositionInput = document.getElementById('toggleTodoPositionInput');
     todoList.toggleCompleted(toggleTodoPositionInput.valueAsNumber);
     toggleTodoPositionInput.value = '';
+    view.displayTodos();
   },
   toggleAll: function() {
     todoList.toggleAll();
+    view.displayTodos();
   }
 };
 
@@ -112,7 +94,8 @@ let view = {
       let todoLi = document.createElement('li');
       let todo = todoList.todos[i];
       let todoTextWithCompletion = '';
-      
+
+      //add appropriate text to show if the todo has been completed.
       if (todo.completed === true) {
         todoTextWithCompletion = `(X) ${todo.todoText}`;
       } else {
